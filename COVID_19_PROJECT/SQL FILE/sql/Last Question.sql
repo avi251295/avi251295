0f9107c1168be14e771b0827f4a9dcb34868ec23
Avinash Kumar <120267658+avi251295@users.noreply.github.com>
2023-09-12 18:40:51 +0530

    Delete COVID_19_PROJECT directory

--- COVID_19_PROJECT/SQL FILE/sql/Last Question.sql
+++
@@ -1,5 +0,0 @@
-select state,DateName(month, date) as month_name,DATEPART(month,date) as month_num,SUM(total_confirmed) as no_of_cases
-from time_series
-group by DATEName(month, date),state,DATEPART(month,date)
-order by month_num,no_of_cases desc;
-
